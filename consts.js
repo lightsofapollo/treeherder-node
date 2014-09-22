6cfe89adad4007b7b271d0b95c1202b71ebd1e0b
James Lal <james@lightsofapollo.com>
2014-09-21 22:13:19 -0700

    Update to https

--- consts.js
+++ consts.js
@@ -3,5 +3,5 @@ Defaults for the treeherder service.
 */
 module.exports = {
   baseUrl: process.env.TREEHERDER_URL ||
-           'http://treeherder-dev.allizom.org/api/'
+           'https://treeherder-dev.allizom.org/api/'
 };
